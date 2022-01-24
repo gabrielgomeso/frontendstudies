1fc41ef362a959239597dc8b169c67bc96caa6c8
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-01-24 12:27:41 -0300

    :sparkles: Finish QR Code challenge

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 
 <template>
   <link rel="icon" type="image/x-icon" href="../assets/favicon-32x32.png">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet"> 
   <TheNavbar></TheNavbar>
   <router-view></router-view>
 </template>
--- src/pages/QrCode.vue
+++ src/pages/QrCode.vue
@@ -7,15 +7,19 @@
           src="../assets/image-qr-code.png"
           alt="QR Code to Frontend Mentor"
         />
-        <figcaption>An elephant at sunset</figcaption>
       </figure>
-      <h3 class="card-title">
+      <h1 class="card-title">
         Improve your front-end skills by building projects
-      </h3>
+      </h1>
       <p class="card-description">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
+    </div>
+
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://www.gabrielgo.dev" target="_blank">Gabriel Gomes</a>.
     </div>
   </section>
 </template>
@@ -25,5 +29,59 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.main {
+  background-color: hsl(212, 45%, 89%);
 
+  width: 100vw;
+  height: calc(100vh - 18px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 5rem;
+
+  text-align: center;
+
+  padding: 1.25rem;
+
+  .card {
+    max-width: 320px;
+    padding: 1.25rem;
+
+    background-color: hsl(0, 0%, 100%);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    font-family: 'Outfit', sans-serif;
+    text-align: center;
+
+    border-radius: 15px;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.05);
+
+    &-image {
+      max-width: 290px;
+      border-radius: 15px;
+    }
+
+    &-title{
+      padding: 15px 10px;
+
+      font-weight: 700;
+      font-size: 22px;
+      color: hsl(218, 44%, 22%);
+      
+    }
+
+    &-description {
+      padding: 0px 10px 20px 10px;
+
+      color: hsl(220, 15%, 55%);
+      font-weight: 400;
+      font-size: 15px;
+    }
+  }
+}
 </style>
